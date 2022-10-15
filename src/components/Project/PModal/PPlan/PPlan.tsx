ceae25af7f706dd3ce3c8cd79c033ee412c0bf2d
Igor Iugin <gigorsaurus@gmail.com>
2022-10-15 18:03:35 +0300

    Добавил управление задачами

--- src/components/Project/PModal/PPlan/PPlan.tsx
+++ src/components/Project/PModal/PPlan/PPlan.tsx
@@ -1,12 +1,71 @@
 import {FC} from 'react'
 
 import {PPlanProps} from './PPlan-props'
+import {StyledGantt} from '../../StyledGantt'
+import {useProject} from '../../Project'
+import {Task} from 'gantt-task-react'
 
+
+function getStartEndDateForProject(tasks: Task[], projectId: string) {
+	const projectTasks = tasks.filter(t => t.project === projectId)
+	let start = projectTasks[0].start
+	let end = projectTasks[0].end
+
+	for (let i = 0; i < projectTasks.length; i++) {
+		const task = projectTasks[i]
+		if (start.getTime() > task.start.getTime()) {
+			start = task.start
+		}
+		if (end.getTime() < task.end.getTime()) {
+			end = task.end
+		}
+	}
+	return [start, end]
+}
 
 export const PPlan: FC<PPlanProps> = () => {
-	return (
-		<>
+	const {tasks, setTasks} = useProject()
+	const handleTaskChange = (task: Task) => {
+		let newTasks = tasks.map(t => (t.id === task.id ? task : t))
+		if (task.project) {
+			const [start, end] = getStartEndDateForProject(newTasks, task.project)
+			const project = newTasks[newTasks.findIndex(t => t.id === task.project)]
+			if (
+				project.start.getTime() !== start.getTime() ||
+				project.end.getTime() !== end.getTime()
+			) {
+				const changedProject = {...project, start, end}
+				newTasks = newTasks.map(t =>
+					t.id === task.project ? changedProject : t
+				)
+			}
+		}
+		setTasks(newTasks)
+	}
 
-		</>
+	const handleTaskDelete = (task: Task) => {
+		const conf = window.confirm(`Подтвердите удаление задачи "${task.name}" ?`)
+		if (conf) {
+			setTasks(tasks.filter(t => t.id !== task.id))
+		}
+		return conf
+	}
+
+	const handleProgressChange = async (task: Task) => {
+		setTasks(tasks.map(t => (t.id === task.id ? task : t)))
+	}
+
+	const handleExpanderClick = (task: Task) => {
+		setTasks(tasks.map(t => (t.id === task.id ? task : t)))
+	}
+
+	return (
+		<StyledGantt
+			tasks={tasks}
+			onDateChange={handleTaskChange}
+			onDelete={handleTaskDelete}
+			onProgressChange={handleProgressChange}
+			onExpanderClick={handleExpanderClick}
+		/>
 	)
 }
--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,4 +1,4 @@
-import {createContext, Dispatch, FC, SetStateAction, useContext} from 'react'
+import {createContext, Dispatch, FC, SetStateAction, useContext, useState} from 'react'
 
 import {ProjectProps} from './Project-props'
 import {Flex, useDisclosure} from '@chakra-ui/react'
@@ -6,17 +6,22 @@ import {IProject} from '../../mocks/mockProjects'
 import {PHeader} from './PHeader'
 import {StyledGantt} from './StyledGantt'
 import {PModal} from './PModal'
+import {Task} from 'gantt-task-react'
 
 
-const ProjectContext = createContext<IProject & { onOpen: Dispatch<SetStateAction<boolean>> }>(null!)
+const ProjectContext = createContext<IProject & {
+	onOpen: Dispatch<SetStateAction<boolean>>
+	setTasks: Dispatch<SetStateAction<Task[]>>
+}>(null!)
 
 export const Project: FC<ProjectProps> = ({data}) => {
+	const [tasks, setTasks] = useState<Task[]>(data.tasks)
 	const {isOpen, onOpen, onClose} = useDisclosure()
 	return (
-		<ProjectContext.Provider value={{...data, onOpen}}>
+		<ProjectContext.Provider value={{...data, tasks, setTasks, onOpen}}>
 			<Flex flexDir='column' gap={6}>
 				<PHeader/>
-				<StyledGantt tasks={data.tasks}/>
+				<StyledGantt tasks={tasks}/>
 			</Flex>
 			<PModal isOpen={isOpen} onClose={onClose}/>
 		</ProjectContext.Provider>
