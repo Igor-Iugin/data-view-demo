a9727a1455e5cc2213ed03492a145ddb73fcc1e9
Igor Iugin <gigorsaurus@gmail.com>
2022-10-15 18:56:28 +0300

    Исправил отображение разных проектов

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,7 @@
 import {FC, useMemo, useState} from 'react'
 
 import {DashboardProps} from './Dashboard-props'
-import {Tab, TabList, TabPanel, TabPanels, Tabs, useColorMode} from '@chakra-ui/react'
+import {Stack, Tab, TabList, TabPanel, TabPanels, Tabs} from '@chakra-ui/react'
 import {Project} from '../../components'
 import {mockProjects} from '../../mocks/mockProjects'
 
@@ -9,23 +9,21 @@ import {mockProjects} from '../../mocks/mockProjects'
 export const Dashboard: FC<DashboardProps> = () => {
 	const [tabIndex, setTabIndex] = useState<number>(0)
 	const onTabChange = (index: number) => setTabIndex(index)
-
-	const {colorMode} = useColorMode()
-	const bdColor = colorMode === 'light' ? 'blackAlpha.100' : 'whiteAlpha.300'
-
 	const projects = useMemo(() => mockProjects, [])
 
 	return (
 		<Tabs index={tabIndex} onChange={onTabChange}>
 			<TabList>
-				{projects.map(project => <Tab key={project.name}>АСО</Tab>)}
+				{projects.map(project => <Tab key={project.name}>{project.name}</Tab>)}
 			</TabList>
 
 			<TabPanels pt={4}>
-				{projects[tabIndex].projects.map(project => (
-					<TabPanel key={project.name} border='1px solid' borderColor={bdColor} borderRadius='md'>
-						<Project data={project}/>
-					</TabPanel>
+				{projects.map(project => (
+					<Stack as={TabPanel} dir='column' spacing={6}>
+						{project.projects.map(project => (
+							<Project key={project.name} data={project}/>
+						))}
+					</Stack>
 				))}
 			</TabPanels>
 		</Tabs>
