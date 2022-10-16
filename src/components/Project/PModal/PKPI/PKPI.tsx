882ba3b40539e02a8b5eecd744968b0d6d926a32
Igor Iugin <gigorsaurus@gmail.com>
2022-10-16 21:31:12 +0300

    Убрал чарт

--- src/components/Project/PModal/PKPI/PKPI.tsx
+++ src/components/Project/PModal/PKPI/PKPI.tsx
@@ -3,7 +3,6 @@ import {FC} from 'react'
 import {PKPIProps} from './PKPI-props'
 import {Box, SimpleGrid} from '@chakra-ui/react'
 import {useProject} from '../../Project'
-import {VictoryKPI} from '../../../VictoryKPI'
 import {ChartJSKPI} from '../../../ChartJSKPI'
 
 
@@ -11,9 +10,6 @@ export const PKPI: FC<PKPIProps> = () => {
 	const {plannedEffect: {data}} = useProject()
 	return (
 		<SimpleGrid columns={2}>
-			<Box h='500px'>
-				<VictoryKPI data={data}/>
-			</Box>
 			<Box h='500px'>
 				<ChartJSKPI data={data}/>
 			</Box>
