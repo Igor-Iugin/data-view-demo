c3bdb383c67e25d31d79ed99a48e662dd07e0a3d
Igor Iugin <gigorsaurus@gmail.com>
2022-10-17 12:22:16 +0300

    Убрал Layout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,7 @@
-import {Layout} from '../../layout'
 import {TableSmart} from '../../components'
 
 export const Home = () => {
 	return (
-		<Layout>
-			<TableSmart/>
-		</Layout>
+		<TableSmart/>
 	)
 }
