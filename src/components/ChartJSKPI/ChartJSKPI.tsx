84e85b46fd23bb2093cf259b33e5d96ef7b4ef9f
Igor Iugin <gigorsaurus@gmail.com>
2022-10-17 19:39:43 +0300

    Стилизовал и поправил LineChart

--- src/components/ChartJSKPI/ChartJSKPI.tsx
+++ src/components/ChartJSKPI/ChartJSKPI.tsx
@@ -3,6 +3,7 @@ import {Chart as ChartJS, ChartOptions, registerables} from 'chart.js'
 import {Chart} from 'react-chartjs-2'
 
 import {ChartJSKPIProps} from './ChartJSKPI-props'
+import {useColorMode, useToken} from '@chakra-ui/react'
 
 
 const monthNames = [
@@ -21,23 +22,60 @@ const monthNames = [
 ]
 
 ChartJS.register(...registerables)
-const options: ChartOptions<'line'> = {
-	responsive: true,
-	locale: 'ru-RU'
-}
 
 export const ChartJSKPI: FC<ChartJSKPIProps> = ({data}) => {
-	const chartRef = useRef<any>(null!)
+	const {colorMode} = useColorMode()
+	const [blackA100, whiteA300, gray800] = useToken('colors', ['blackAlpha.100', 'whiteAlpha.300', 'gray.800'])
+	const gridColor = colorMode === 'light' ? blackA100 : whiteA300
+	const textColor = colorMode === 'light' ? gray800 : 'white'
+	const options: ChartOptions<'line'> = {
+		locale: 'ru-RU',
+		plugins: {
+			datalabels: {
+				display: true,
+				font: {size: 16},
+				color: 'white',
+				formatter: value => value.y
+			},
+			tooltip: {enabled: false},
+			title: {
+				text: 'Время, затраченное на предоставление услуги',
+				display: true,
+				fullSize: true,
+				font: {size: 20},
+				padding: {bottom: 20},
+				color: textColor,
+			},
+			legend: {
+				labels: {color: textColor},
+				position: 'bottom'
+			},
+		},
+		scales: {
+			x: {
+				grid: {display: false},
+				ticks: {color: textColor}
+			},
+			y: {
+				grid: {color: gridColor},
+				ticks: {color: textColor}
+			},
+		},
+		elements: {
+			bar: {borderRadius: 10}
+		},
+	}
 	const newData = {
 		labels: monthNames,
 		datasets: [{
-			label: 'Время, затраченное на предоставление услуги',
+			label: 'Время',
 			data: data,
 			borderColor: 'rgb(53, 162, 235)',
 			backgroundColor: 'rgba(53, 162, 235, 0.5)',
 		}]
 	}
 
+	const chartRef = useRef<any>(null!)
 	useEffect(() => {
 		return () => {
 			chartRef.current = null
